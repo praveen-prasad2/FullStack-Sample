098570620eb440f943686b98e542aa89ee1a937c
Praveen Prasad <praveenprasad2510@gmail.com>
2023-03-30 08:10:16 +0530

    completed Project

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -1,14 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Signup from './Components/Signup'
 import Signin from './Components/Signin/Signin'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
 
 function App() {
 
   return (
-    <Signup />
+    
+    <BrowserRouter>
+    <Routes>
+      <Route path='' element={<Signup />}/>
+      <Route path='login' element={<Signin  />}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
--- Front-End/src/Components/Signup.jsx
+++ Front-End/src/Components/Signup.jsx
@@ -1,16 +1,42 @@
 import React from 'react'
 import { TextField,Box,Button } from '@mui/material'
 import './Signup/Signup.css'
+import { useRef } from 'react'
+import axios from 'axios'
+import {  useNavigate } from 'react-router-dom'
 
 function signup() {
+
+  const usernameRef=useRef()
+  const emailRef=useRef()
+  const pass1Ref=useRef()
+  const pass2Ref=useRef()
+
+  const navigate=useNavigate()
+
+  async function signupUser(){
+    if(pass1Ref.current.value==pass2Ref.current.value){
+
+      let newuser={
+        username:usernameRef.current.value,
+        email:emailRef.current.value,
+        password:pass1Ref.current.value
+  
+      }
+      let response=await axios.post("http://localhost:5000/user",newuser)
+      navigate('/login')
+    }else{
+      alert("Password Missv  match")
+    }
+  }
   return (
    <Box id="wrap">
      <h1>Sign Up</h1>
-    <TextField id="standard-basic" label="Full Name" variant="standard" type="text" /><br /><br />
-    <TextField id="standard-basic" label="Email" variant="standard" type="email" /><br /><br />
-    <TextField id="standard-basic" label="Password" variant="standard" type="password" /><br /><br />
-    <TextField id="standard-basic" label="Re-enter Password" variant="standard" type="email" /><br /><br />
-    <Button variant="outlined">SignUp</Button>
+    <TextField id="standard-basic" label="Full Name" variant="standard" type="text" inputRef={usernameRef}/><br /><br />
+    <TextField id="standard-basic" label="Email" variant="standard" type="email" inputRef={emailRef}/><br /><br />
+    <TextField id="standard-basic" label="Password" variant="standard" type="password" inputRef={pass1Ref}/><br /><br />
+    <TextField id="standard-basic" label="Re-enter Password" variant="standard" type="password" inputRef={pass2Ref}/><br /><br />
+    <Button variant="outlined" onClick={signupUser}>SignUp</Button>
    </Box>
   )
 }
